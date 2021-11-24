26d0b6d63b12b6f0a6c4651742842f29d12f4d42
mikehong <nyct>
2021-11-23 23:40:06 -0500

    lab 4 pinger

--- solution.py
+++ solution.py
@@ -259,7 +259,6 @@
 
 
 from socket import *
-import socket
 import os
 import sys
 import struct
@@ -267,7 +266,6 @@ import time
 import select
 import binascii
 
-# use mac or linux terminal to run code using sudo Python testing.py
 ICMP_ECHO_REQUEST = 8
 MAX_HOPS = 30
 TIMEOUT = 2.0
@@ -277,20 +275,20 @@ TRIES = 2
 # The packet that we shall send to each router along the path is the ICMP echo
 # request packet, which is exactly what we had used in the ICMP ping exercise.
 # We shall use the same packet that we built in the Ping exercise
-
-def checksum(str_):
+def checksum(str):
     # In this function we make the checksum of our packet
-    str_ = bytearray(str_)
+    # hint: see icmpPing lab
     csum = 0
-    countTo = (len(str_) // 2) * 2
-
-    for count in range(0, countTo, 2):
-        thisVal = str_[count + 1] * 256 + str_[count]
+    countTo = (len(str) // 2) * 2
+    count = 0
+    while count < countTo:
+        thisVal = str[count + 1] * 256 + str[count]
         csum = csum + thisVal
         csum = csum & 0xffffffff
+        count = count + 2
 
-    if countTo < len(str_):
-        csum = csum + str_[-1]
+    if countTo < len(str):
+        csum = csum + str[len(str) - 1]
         csum = csum & 0xffffffff
 
     csum = (csum >> 16) + (csum & 0xffff)
@@ -302,48 +300,53 @@ def checksum(str_):
 
 
 def build_packet():
-    # In the sendOnePing() method of the ICMP Ping exercise ,firstly the header of our
+    # In the sendOnePing() method of the ICMP Ping exercise, firstly the header of our
     # packet to be sent was made, secondly the checksum was appended to the header and
     # then finally the complete packet was sent to the destination.
-
     # Make the header in a similar way to the ping exercise.
-    myChecksum = 0
-    myID = os.getpid() & 0xFFFF
+    # Append checksum to the header.
+    # Don't send the packet yet , just return the final packet in this function.
+    # So the function ending should look like this packet = header + data return packet
 
+    ID = os.getpid() & 0xFFFF  # Return the current process i
+    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+    myChecksum = 0
     # Make a dummy header with a 0 checksum.
     # struct -- Interpret strings as packed binary data
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
-    # header = struct.pack("!HHHHH", ICMP_ECHO_REQUEST, 0, myChecksum, pid, 1)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     data = struct.pack("d", time.time())
-
     # Calculate the checksum on the data and the dummy header.
-    # Append checksum to the header.
     myChecksum = checksum(header + data)
+    # Get the right checksum, and put in the header
     if sys.platform == 'darwin':
-        myChecksum = socket.htons(myChecksum) & 0xffff
+        myChecksum = htons(myChecksum) & 0xffff
         # Convert 16-bit integers from host to network byte order.
     else:
         myChecksum = htons(myChecksum)
 
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
+
     return packet
 
 
 def get_route(hostname):
-    timeLeft = TIMEOUT
+    # timeLeft = TIMEOUT     # Is this line in the wrong place? I changed it to three lines below......
+    print("Begin traceroute to " + hostname + "(" + gethostbyname(hostname) + ")......\n")
+
     for ttl in range(1, MAX_HOPS):
         for tries in range(TRIES):
-            destAddr = socket.gethostbyname(hostname)
-
+            timeLeft = TIMEOUT
+            destAddr = gethostbyname(hostname)
             # Fill in start
             # Make a raw socket named mySocket
-            icmp = socket.getprotobyname("icmp")
-            # mySocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-            mySocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, icmp)
+            icmp = getprotobyname("icmp")
+            try:
+                mySocket = socket(AF_INET, SOCK_RAW, icmp)
+            except error as msg:
+                print("Socket create error:", msg)
             # Fill in end
-
-            mySocket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, struct.pack('I', ttl))
+            mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
             mySocket.settimeout(TIMEOUT)
             try:
                 d = build_packet()
@@ -352,62 +355,63 @@ def get_route(hostname):
                 startedSelect = time.time()
                 whatReady = select.select([mySocket], [], [], timeLeft)
                 howLongInSelect = (time.time() - startedSelect)
-
                 if whatReady[0] == []:  # Timeout
-                    print("*    *    * Request timed out.")
-
+                    print("\t*\t\t*\t\t*\t\tRequest timed out.")
                 recvPacket, addr = mySocket.recvfrom(1024)
-                print(addr)
                 timeReceived = time.time()
+
                 timeLeft = timeLeft - howLongInSelect
-
                 if timeLeft <= 0:
-                    print("*    *    * Request timed out.")
-
-            except socket.timeout:
+                    print("\t*\t*\t*\Request timed out.")
+            except timeout:
                 continue
-
             else:
-                icmpHeader = recvPacket[20:28]
-                request_type, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
+                # Fill in start
+                # Fetch the icmp type from the IP packet
 
-                if request_type == 11:
+                # get TTL
+                ttl = recvPacket[8]
+                # get ICMP info
+                type, pongCode, pongChecksum, pongID, pongSequence = struct.unpack("bbHHh", recvPacket[20:28])
+                # get RTT in ms
+                RTT = (timeReceived - struct.unpack("d", recvPacket[28:36])[0]) * 1000
+
+                # try to get hostname of each router in the path
+                try:
+                    routerHostname = gethostbyaddr(addr[0])[0]
+                except herror as emsg:
+                    routerHostname = "(Could not look up name:" + str(emsg) + ")"
+
+                # Fill in end
+                if type == 11:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    print(" %d   rtt=%.0f ms %s" % (ttl, (timeReceived - t) * 1000, addr[0]))
-                elif request_type == 3:
+                    print("TTL = %d\trtt=%.0f ms\tIP = %s\tHost:%s" % (
+                    ttl, (timeReceived - t) * 1000, addr[0], routerHostname))
+                elif type == 3:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    print(" %d   rtt=%.0f ms %s" % (ttl, (timeReceived - t) * 1000, addr[0]))
-                elif request_type == 0:
+                    print("TTL = %d\trtt=%.0f ms\tIP = %s\tHost:%s" % (
+                    ttl, (timeReceived - t) * 1000, addr[0], routerHostname))
+                elif type == 0:
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
-                    print(" %d   rtt=%.0f ms %s" % (ttl, (timeReceived - timeSent) * 1000, addr[0]))
+                    print("TTL = %d\trtt=%.0f ms\tIP = %s\tHost:%s" % (
+                    ttl, (timeReceived - timeSent) * 1000, addr[0], routerHostname))
                     return
                 else:
                     print("error")
-                    break
+                break
             finally:
                 mySocket.close()
 
 
-'''
-print("--------------------------------------------")                
-print ('www.google.com')
-print("--------------------------------------------")
-get_route("www.google.com") # USA - North America
-print("--------------------------------------------")
-print ('www.china.org.cn')
-print("--------------------------------------------")
-get_route('www.china.org.cn') # China - Asia
-print("--------------------------------------------")
-print ('www.sweden.se')
-print("--------------------------------------------")
-get_route('www.sweden.se') # Sweden - Europe
-print("--------------------------------------------")
-print ('www.mybroadband.co.za')
-print("--------------------------------------------")
-get_route('www.mybroadband.co.za') # some place in Africa
-'''
-
-get_route('google.com')
+# traceroute four different host
+get_route("www.baidu.com")
+print("Traceroute Finished!\n\n\n\n\n\n")
+get_route("www.google.com")
+print("Traceroute Finished!\n\n\n\n\n\n")
+get_route("www.tsinghua.edu.cn")
+print("Traceroute Finished!\n\n\n\n\n\n")
+get_route("www.github.com")
+print("Traceroute Finished!\n\n\n\n\n\n")
